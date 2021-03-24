ad45315a08d107930576e1f2c497cf424a8c2cc6
andybrvt <andybrvt@gmail.com>
2021-03-24 00:00:52 -0700

    backedn try for mobile

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { enableScreens } from 'react-native-screens';
 import { createNativeStackNavigator } from 'react-native-screens/native-stack';
 import Routes from './Routes'
 import { NavigationContainer } from '@react-navigation/native';
+import axios from "axios";
+// import * as dateFns from 'date-fns';
 
 
 
@@ -13,6 +15,18 @@ class App extends Component{
     super(props)
   }
 
+
+  componentDidMount(){
+      fetch("http://127.0.0.1:8000/userprofile/all-users", {
+        method:"GET"
+      })
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err =>{
+        console.log(err)
+      })
+  }
 
   render(){
 
--- Home.js
+++ Home.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { Text, View, Button } from 'react-native';
+import axios from "axios";
+
 
 class Home extends React.Component{
 
+
+  // componentDidMount(){
+  //     axios.get("http://127.0.0.1:8000/userprofile/testMobileView")
+  //     .then(res => {
+  //       console.log(res)
+  //     })
+  //     .catch(err =>{
+  //       console.log(err)
+  //     })
+  // }
 
   render(){
     console.log(this.props)
