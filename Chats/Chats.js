8a861acc07291812366d4243e784ac377db923ed
andybrvt <andybrvt@gmail.com>
2021-04-21 01:16:08 -0700

    rendering chats into new js files

--- Chats/Chats.js
+++ Chats/Chats.js
@@ -3,6 +3,8 @@ import { Text, View, Button,StyleSheet } from 'react-native';
 import axios from "axios";
 import * as authActions from '../store/actions/auth';
 import { connect } from 'react-redux';
+import { FlatList } from "react-native-bidirectional-infinite-scroll";
+
 
 
 // The first thing you want to do in making the chat is
@@ -12,14 +14,42 @@ import { connect } from 'react-redux';
 
 // First you want to link up your chat sidepanel with its own websocket
 // so that you can get incoming messages
+
+// this will be used to show the list of chats
 class Chats extends React.Component{
 
 
-  render(){
-    console.log(this.props)
+
+  renderItem = ({item}) => {
+    console.log('these are individual items')
+    console.log(item)
     return (
       <View>
-        <Text> This will be the chats</Text>
+        <Text> test</Text>
+      </View>
+    )
+  }
+
+  render(){
+
+    let chatList = []
+    let seenList = []
+
+    if(this.props.chats){
+      chatList = this.props.chats
+    }
+
+    // You are gonna use a flat list for this chat
+    return (
+      <View>
+
+        <FlatList
+          data = {chatList}
+          renderItem = {this.renderItem}
+          keyExtractor={(item) => item.id}
+
+
+          />
       </View>
 
 
