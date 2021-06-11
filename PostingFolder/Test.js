c5c365ee2bda8064d02093aa31bbba19b01b01f8
andybrvt <andybrvt@gmail.com>
2021-06-10 23:54:55 -0700

    loop testing working

--- PostingFolder/Test.js
+++ PostingFolder/Test.js
@@ -32,6 +32,7 @@ import { PanGestureHandler, State } from "react-native-gesture-handler";
 import { ArrowUpCircle, Plus, Mail, UserPlus, X, XCircle, PlusCircle } from "react-native-feather";
 import { SCREEN_HEIGHT, SCREEN_WIDTH} from "../Constants";
 import {loop, withTimingTransition, timing, mix} from 'react-native-redash/lib/module/v1';
+import { useMemoOne } from "use-memo-one";
 
 
 const width = Dimensions.get("window").width
@@ -40,7 +41,9 @@ const margin = 0;
 const col = 3;
 const size = width/col + margin;
 
-const { cond, sub,divide, eq, add, call, set, Value, event, or, Clock, useCode } = Animated;
+const { cond, sub,divide, eq, add, call, set, Value, event, or, Clock, useCode, block, and, not, clockRunning,
+  startClock,
+  stopClock } = Animated;
 
 const isHidden = true;
 
@@ -51,37 +54,56 @@ const isHidden = true;
 //     expanded: false
 //   }
 //
+//   expanded = false;
 //   animation = new Value(this.state.expanded ? 1 : 0)
 //   clock = new Clock()
 //
 //   componentDidMount(){
-//     this.animation.setValue(
-//       timing(this.clock,this.animation,{
-//         toValue: this.state.expanded ? 0 : 1,
-//         duration: 4000,
-//         easing: Easing.inOut(Easing.ease)
-//       }))
+//
 //   }
 //
-//   scale = mix(this.animation, 0.4, 1);
-//   rotate = mix(this.animation, 0, 2*3.15 *5)
+//   changeState = () => {
+//     const {expanded} = this.state
+//
+//     this.setState({
+//       expanded: !expanded
+//     })
+//
+//   }
+//
+//   scale = mix(this.animation, 0.1, 1);
+//   rotate = mix(this.animation, 0, 2*Math.PI *5)
 //
 //   render(){
 //     return(
-//       <TouchableWithoutFeedback onPress = {() => expand(!expanded)}>
-//         <Animated.View style = {{
-//             height: 100,
-//             width: 100,
-//             backgroundColor: "black",
-//             transform: [
-//               {scale: this.scale}
-//             ]
-//           }}>
+//       <View>
+//         <Animated.Code>
+//           {() =>
+//             set(this.animation, timing(this.clock,this.animation,{
+//                     toValue: 1,
+//                     duration: 4000,
+//                     easing: Easing.inOut(Easing.ease)
+//                   }))
+//             }
+//         </Animated.Code>
+//         <TouchableWithoutFeedback onPress = {() => this.changeState()}>
+//           <Animated.View style = {{
+//               height: 100,
+//               width: 100,
+//               backgroundColor: "black",
+//               transform: [
+//                 {scale: this.scale},
+//                 {rotate: this.rotate}
+//
+//               ]
+//             }}>
 //
 //
-//         </Animated.View>
+//           </Animated.View>
 //
-//       </TouchableWithoutFeedback>
+//         </TouchableWithoutFeedback>
+//       </View>
+//
 //     )
 //   }
 // }
@@ -90,31 +112,57 @@ const isHidden = true;
 
 export default function Test(){
 
-  const [expanded, expand] = useState(false);
+  const [play, setPlay] = useState(false);
 
-  const animation = new Value(expanded ? 1 : 0);
-  const clock = new Clock()
-
-  // useCode(set(animation, timing(clock,animation,{
-  //         toValue: expanded ? 0 : 1,
-  //         duration: 4000,
-  //         easing: Easing.inOut(Easing.ease)
-  //       })),[animation])
-
-  useCode(set(animation, new Value(0)),[animation])
+  const { isPlaying, animation, clock } = useMemoOne(
+      () => ({
+        isPlaying: new Value(0),
+        animation: new Value(0),
+        clock: new Clock()
+      }),
+      []
+    );
 
 
-  const scale = mix(animation, 0, 1);
-  const rotate = mix(animation, 0, 2 * Math.PI* 5);
+  useCode(
+    block([
+      cond(and(not(clockRunning(clock)), isPlaying), startClock(clock)),
+      cond(and(clockRunning(clock), not(isPlaying)), stopClock(clock)),
+      set(
+        animation,
+        loop({
+          clock,
+          duration: 4000,
+          easing: Easing.inOut(Easing.ease),
+          boomerang: true,
+          autoStart: false
+        })
+      )
+    ]),
+    []
+  );
+
+  const scale = mix(animation, 0.4, 1);
+ const rotate = mix(animation, 0, 2 * Math.PI * 5);
+
 
 
 
   return (
-    <TouchableWithoutFeedback onPress = {() => expand(!expanded)}>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        setPlay(!play);
+        isPlaying.setValue(play ? 0 : 1);
+      }}
+      >
            <Animated.View style = {{
                height: 100,
                width: 100,
                backgroundColor: "black",
+               transform: [
+                 {scale: scale},
+                 {rotate: rotate }
+               ]
              }}>
 
 
