65291899e08e5838224ffb502f8f042e381c6ae4
pinghsu520 <pinghsu520@gmail.com>
2021-07-12 21:21:13 -0700

    notification start

--- ProfilePage/Notifications.js
+++ ProfilePage/Notifications.js
@@ -11,7 +11,10 @@ import BackgroundContainer from '../RandomComponents/BackgroundContainer';
  // this class will be a page on its own where
  // you can upload pictures and write a caption after uploaidng
  // pictures
+ import { Avatar } from 'react-native-elements';
 import { FlatList } from "react-native-bidirectional-infinite-scroll";
+import { MoreVertical
+} from "react-native-feather";
  class Notifications extends React.Component{
 
    onHomeNav = () => {
@@ -20,9 +23,42 @@ import { FlatList } from "react-native-bidirectional-infinite-scroll";
    }
 
    renderItem = ({item}) => {
+     // like notification
+     // comment Notification
+     // private -- accept/decline friend Notification
+     // public -- somone follows you
+
      console.log("")
      return (
-       <View><Text>{item.username} liked your post on</Text></View>
+       <View style={{flexDirection:'row', padding:15}}>
+         <View style={{flex:1}}>
+           <Avatar
+             // onPress = {() => this.props.ViewProfile(userUsername)}
+             size={40}
+             rounded
+             source = {{
+               uri: item.pic
+             }}
+           />
+         </View>
+         <View style={{flex:6,  flexDirection:'row'}}>
+           <Text style={{fontWeight:'bold'}}> {item.username}</Text>
+           <Text> liked your album on</Text>
+           <Text style={{fontWeight:'bold'}}> July 1</Text>
+          </View>
+        <View style={{flex:0}}>
+        <Text style={{}}> {item.time}</Text>
+        {/*
+        <MoreVertical
+        style={{top:10}}
+          stroke="gray"
+          width ={25}
+          height = {25}
+        />
+        */}
+
+        </View>
+       </View>
      )
    }
    FlatListItemSeparator = () => {
@@ -38,13 +74,20 @@ import { FlatList } from "react-native-bidirectional-infinite-scroll";
    }
 
    render(){
-     const chatList=[{"username":"pinghsu520"
-     , "second":1}, {"username":"andybrvt"}]
+     const chatList=[
+       {"username":"pinghsu520",
+       "pic":"http://192.168.1.200:19002/media/PostPic/public/profile_pictures/2021/03/satsifying_2.png",
+       "time": "3d",
+       },
+     {"username":"andybrvt",
+       "pic":"http://192.168.1.200:19002/media/PostPic/public/profile_pictures/2021/06/doggy.jpeg",
+       "time": "5d",
+      }
+   ]
 
      return (
        <BackgroundContainer>
          <View >
-           <Text> noti gang</Text>
            <FlatList
              style = {{marginTop:5}}
              data = {chatList}
