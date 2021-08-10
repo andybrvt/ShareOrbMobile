e6e21979ec8bace14ae61ca8afd8146eeb97997b
andybrvt <andybrvt@gmail.com>
2021-08-10 12:10:32 -0700

    setup animation for caption on camera screen

--- RandomComponents/InputModal.js
+++ RandomComponents/InputModal.js
@@ -12,23 +12,77 @@ import {
   TouchableOpacity,
   TouchableHighlight,
   TouchableWithoutFeedback,
-  Modal
+  Modal,
+  KeyboardAvoidingView
  } from 'react-native';
+ const width = Dimensions.get("window").width
+ const height = Dimensions.get('window').height
 
-// This will be used for input modal such as captions
+ class InputModal extends React.Component{
 
 
-class InputModal extends React.Component{
 
-  render(){
-    return(
-      <View>
-          
-      </View>
-    )
+   onCancel = () => {
+     this.props.onCancel()
+   }
+
+   onAction = () => {
+     this.props.onAction()
+   }
+
+   render(){
+     return(
+       <KeyboardAvoidingView
+         style = {{
+           padding: 20,
+           backgroundColor: 'white',
+           width: width*0.8,
+           alignItems: 'center'
+         }}
+         behavior = "height"
+         keyboardVerticalOffset = {100}
+         >
+
+        <Text>Write a caption here today</Text>
+        <View
+          style = {{
+            backgroundColor: "whitesmoke",
+            padding: 20,
+            borderRadius:10,
+            width: "100%"
+          }}
+          >
+
+          <TextInput
+            placeholder = "Write something here"
+            multiline = {true}
+             />
+        </View>
+
+      </KeyboardAvoidingView>
+     )
+   }
+ }
+
+const styles = StyleSheet.create({
+  button: {
+    position: "relative",
+    width: "90%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
+    backgroundColor: "#1890ff",
+
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 14
+  },
+  cancelText: {
+    marginTop: 20,
   }
+})
 
-}
-
-
-export dafault InputModal;
+export default InputModal;
