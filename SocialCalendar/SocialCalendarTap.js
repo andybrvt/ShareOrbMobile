a43c4c821c9833d2fdd88226c01ff1df43d54538
andybrvt <andybrvt@gmail.com>
2021-08-11 15:17:47 -0700

    android image load up

--- SocialCalendar/SocialCalendarTap.js
+++ SocialCalendar/SocialCalendarTap.js
@@ -30,72 +30,110 @@ class SocialCalendarTap extends React.Component{
 
   }
 
-  addMonth = ()=>{
-    // this function will increaes month by one
-    this.setState({
-      currentDate: dateFns.addMonths(this.state.currentDate, 1),
-      socialCells: {}
-    }, () => {
-      const range = this.grabMonth()
-      this.getSocialCells(range.start, range.end)
-    })
+  viewDay = (cellId) => {
 
-  }
-
-  subMonth =() =>{
-    // this function will decrase the month by one
-    this.setState({
-      currentDate: dateFns.subMonths(this.state.currentDate, 1),
-      socialCells: {}
-    }, () => {
-      const range = this.grabMonth()
-      this.getSocialCells(range.start, range.end)
-
-    })
-
-  }
-
-  getSocialCells(start,end){
-    const userId = this.props.userId;
-    const newStart = dateFns.format(start, "yyyy-MM-dd")
-    const newEnd = dateFns.format(end, "yyyy-MM-dd")
-    return authAxios.get(`${global.IP_CHANGE}/mySocialCal/filterCells/`+userId+"/"+ newStart+`/`+newEnd)
-    .then(res => {
-      this.setState({
-        socialCells: res.data
+      this.props.navigation.navigate("DayAlbum", {
+        cellId: cellId
       })
-    })
-  }
-
-  grabMonth(){
-    const startMonth = dateFns.startOfMonth(this.state.currentDate)
-    const endMonth = dateFns.endOfMonth(this.state.currentDate)
-
-    const start = dateFns.startOfWeek(startMonth);
-    const end = dateFns.endOfWeek(endMonth);
-
-    const diffWeeks = dateFns.differenceInCalendarWeeks(end, start)
-
-    if(diffWeeks === 4){
-      return{
-        start: start,
-        end: dateFns.addWeeks(end,1)
-      }
-    }
-
-    return{
-      start: start,
-      end: end
-    }
   }
 
   componentDidMount(){
+    const dateRange = this.grabDateRange()
 
-    const range = this.grabMonth(this.state.currentDate)
-    this.getSocialCells(range.start, range.end)
+    if(this.props.userId !== 'undefined' && this.props.userId !== ""){
+      this.getSocialCells(dateRange.start, dateRange.end)
+      .then(data => {
+        this.setState({
+          socialCells: data
+        })
+
+      })
+    }
+
+
+
+    const onRefresh = this.props.navigation.addListener("focus", () =>{
+      // console.log('on refresh here')
+      //
+      // console.log(this.grabDateRange())
+      // this.getSocialCells(dateRange.start, dateRange.end)
+      // .then(data => {
+      //   console.log(data.length)
+      //   this.setState({
+      //     socialCells: data
+      //   })
+      //
+      // })
+    })
 
 
   }
+
+
+  componentDidUpdate(prevProps){
+    // when it updates and is on a new months you will get the items of the new
+    // month
+
+    if(this.props !== prevProps){
+      if(this.props.month !== prevProps.month){
+        if(this.props.userId !== ""){
+          const dateRange = this.grabDateRange()
+          this.getSocialCells(dateRange.start, dateRange.end)
+          .then(data => {
+            this.setState({
+              socialCells: data
+            })
+          })
+        }
+      } else if(this.props.userId !== "") {
+
+        const dateRange = this.grabDateRange()
+        this.getSocialCells(dateRange.start, dateRange.end)
+        .then(data => {
+          this.setState({
+            socialCells: data
+          })
+        })
+      }
+    }
+
+
+
+
+
+  }
+
+  grabDateRange(){
+    // this function is used to get the end range of the month
+    const curMonth = new Date(this.props.year, this.props.month, 1);
+    const start = dateFns.startOfMonth(curMonth)
+    const end = dateFns.endOfMonth(curMonth)
+    const startDate = dateFns.startOfWeek(start)
+    const endDate = dateFns.endOfWeek(end)
+    const diffWeeks = dateFns.differenceInCalendarWeeks(endDate, startDate)
+    if(diffWeeks === 4 ){
+      // increase the range by a week
+      const newEndDate = dateFns.addWeeks(endDate, 1);
+      const formatStart = dateFns.format(startDate, 'yyyy-MM-dd')
+      const formatEnd = dateFns.format(newEndDate, 'yyyy-MM-dd')
+      return {start: formatStart, end: formatEnd}
+    } else {
+      const formatStart = dateFns.format(startDate, 'yyyy-MM-dd')
+      const formatEnd = dateFns.format(endDate, 'yyyy-MM-dd')
+      return {start: formatStart, end: formatEnd}
+    }
+  }
+
+
+  getSocialCells(start, end){
+    const userId = this.props.userId;
+    return authAxios.get(`${global.IP_CHANGE}/mySocialCal/filterCells/`+userId+"/"+ start+`/`+end)
+    .then(res => {
+      return res.data
+    })
+  }
+
+
 
   renderHeader(){
 
@@ -146,18 +184,80 @@ class SocialCalendarTap extends React.Component{
 
     if(diffWeeks === 4 ){
       while(day<= dateFns.addWeeks(endDate, 1)){
+
+        for(let item = 0; item < cells.length; item++){
+          const date = new Date(cells[item].socialCaldate)
+          const utc = dateFns.addHours(date, date.getTimezoneOffset()/60)
+
+          if(dateFns.isSameDay(utc, day)){
+            toDoStuff.push(
+              cells[item]
+            )
+          }
+        }
+
         formattedDate = dateFns.format(day, dateFormat);
 
+        if(toDoStuff.length > 0){
+          days.push(
+            <View
 
-        days.push(
-          <View
-            // onPress = {() => this.props.navigation.navigate("PostingPage")}
-            style = {[styles.monthCell]}>
-            {
-              (dateFns.isSameDay(day, new Date()))?
-              <TouchableOpacity onPress = {() => this.props.navigation.navigate("PostingPage")}
-                style = {styles.currentMiniBox}>
-              <View>
+              style={styles.monthPicCell}
+              >
+              <View style = {styles.imageHolder}>
+                {
+                  toDoStuff[0].coverPic ?
+                    <View>
+                      <TouchableOpacity  onPress={() => this.viewDay(info.id)}>
+                        <View style={styles.miniBox}>
+                          <Text style = {styles.formatDateImage}> {formattedDate}</Text>
+                          <Image
+                            style = {dateFns.isSameDay(day, new Date()) ?
+                              styles.smallImageGlow : styles.smallImage
+                            }
+
+                            resizeMode = "cover"
+                            source={{ uri: `${global.IMAGE_ENDPOINT}${toDoStuff[0].coverPic}` }}
+                            />
+                        </View>
+
+                      </TouchableOpacity>
+                    </View>
+                  :
+                    <Text style = {{color: 'red'}}> {formattedDate}</Text>
+                  }
+              </View>
+            </View>
+          )
+        } else {
+          days.push(
+            <View
+              // onPress = {() => this.props.navigation.navigate("PostingPage")}
+              style = {[styles.monthCell]}>
+              {
+                (dateFns.isSameDay(day, new Date()))?
+                <TouchableOpacity onPress = {() => this.props.navigation.navigate("PostingPage")}
+                  style = {styles.currentMiniBox}>
+                <View>
+                  {
+                    dateFns.isSameMonth(day, curMonth) ?
+                    <Text style = {{
+                        fontSize:12,
+                        color: 'black',
+                        fontWeight: 'bold'
+                      }}> {formattedDate}</Text>
+                    :
+                    <Text style = {{
+                        fontSize:12,
+                        color: "#8c8c8c"
+                      }}>{formattedDate}</Text>
+                  }
+                  </View>
+                  </TouchableOpacity>
+                :
+                <View onPress = {() => this.props.navigation.navigate("PostingPage")}
+                  style = {styles.miniBox}>
+                <View>
                 {
                   dateFns.isSameMonth(day, curMonth) ?
                   <Text style = {{
@@ -172,30 +272,13 @@ class SocialCalendarTap extends React.Component{
                     }}>{formattedDate}</Text>
                 }
                 </View>
-                </TouchableOpacity>
-              :
-              <View onPress = {() => this.props.navigation.navigate("PostingPage")}
-                style = {styles.miniBox}>
-              <View>
-              {
-                dateFns.isSameMonth(day, curMonth) ?
-                <Text style = {{
-                    fontSize:12,
-                    color: 'black',
-                    fontWeight: 'bold'
-                  }}> {formattedDate}</Text>
-                :
-                <Text style = {{
-                    fontSize:12,
-                    color: "#8c8c8c"
-                  }}>{formattedDate}</Text>
+                </View>
               }
-              </View>
-              </View>
-            }
-          </View>
-        )
+            </View>
+          )
+        }
 
+        toDoStuff = [];
         day= dateFns.addDays(day,1);
 
 
@@ -203,20 +286,75 @@ class SocialCalendarTap extends React.Component{
     }
 
 
-    } else {
+  } else if(diffWeeks === 5) {
       while(day<= endDate){
+
+        for(let item = 0; item < cells.length; item++){
+          const date = new Date(cells[item].socialCaldate)
+          const utc = dateFns.addHours(date, date.getTimezoneOffset()/60)
+
+          if(dateFns.isSameDay(utc, day)){
+            toDoStuff.push(
+              cells[item]
+            )
+          }
+        }
         formattedDate = dateFns.format(day, dateFormat);
 
-
-        days.push(
-          <View
-            // onPress = {() => this.props.navigation.navigate("PostingPage")}
-            style = {[styles.monthCell]}>
-            {
-              (dateFns.isSameDay(day, new Date()))?
-              <TouchableOpacity onPress = {() => this.props.navigation.navigate("PostingPage")}
-                style = {styles.currentMiniBox}>
-              <View>
+        if(toDoStuff.length > 0){
+          days.push(
+            <View
+              key = {i}
+              style = {[styles.monthPicCell]}>
+              <View style = {[dateFns.isSameDay(day, new Date()) ?
+                styles.currentMiniBox : styles.miniBox
+              ]}>
+                {
+                  toDoStuff[0].coverPic ?
+                    <View>
+                      <TouchableOpacity  onPress={() => this.ViewDay(toDoStuff.id)}>
+                        <Image
+                          style = {styles.smallImage}
+                          resizeMode = "cover"
+                          source={{ uri: `${global.IMAGE_ENDPOINT}${toDoStuff[0].coverPic}` }}
+                          />
+                      </TouchableOpacity>
+                    </View>
+                  :
+                    <Text> {formattedDate}</Text>
+                  }
+              </View>
+            </View>
+          )
+        } else {
+          days.push(
+            <View
+              // onPress = {() => this.props.navigation.navigate("PostingPage")}
+              style = {[styles.monthCell]}>
+              {
+                (dateFns.isSameDay(day, new Date()))?
+                <TouchableOpacity onPress = {() => this.props.navigation.navigate("PostingPage")}
+                  style = {styles.currentMiniBox}>
+                <View>
+                  {
+                    dateFns.isSameMonth(day, curMonth) ?
+                    <Text style = {{
+                        fontSize:12,
+                        color: 'black',
+                        fontWeight: 'bold'
+                      }}> {formattedDate}</Text>
+                    :
+                    <Text style = {{
+                        fontSize:12,
+                        color: "#8c8c8c"
+                      }}>{formattedDate}</Text>
+                  }
+                  </View>
+                  </TouchableOpacity>
+                :
+                <View onPress = {() => this.props.navigation.navigate("PostingPage")}
+                  style = {styles.miniBox}>
+                <View>
                 {
                   dateFns.isSameMonth(day, curMonth) ?
                   <Text style = {{
@@ -231,30 +369,13 @@ class SocialCalendarTap extends React.Component{
                     }}>{formattedDate}</Text>
                 }
                 </View>
-                </TouchableOpacity>
-              :
-              <View onPress = {() => this.props.navigation.navigate("PostingPage")}
-                style = {styles.miniBox}>
-              <View>
-              {
-                dateFns.isSameMonth(day, curMonth) ?
-                <Text style = {{
-                    fontSize:12,
-                    color: 'black',
-                    fontWeight: 'bold'
-                  }}> {formattedDate}</Text>
-                :
-                <Text style = {{
-                    fontSize:12,
-                    color: "#8c8c8c"
-                  }}>{formattedDate}</Text>
+                </View>
               }
-              </View>
-              </View>
-            }
-          </View>
-        )
+            </View>
+          )
+        }
 
+        toDoStuff = [];
         day= dateFns.addDays(day,1);
 
 
@@ -349,7 +470,6 @@ const styles = StyleSheet.create({
 
   },
   monthPicCell: {
-    flex: 1,
     height: Math.round(Dimensions.get('window').width/7),
     alignItems: "center",
     justifyContent: "center",
