feaf741f79794b079df64387572f11f01046ad6d
andybrvt <andybrvt@gmail.com>
2021-08-18 11:53:39 -0700

    small bug fix in social calendar

--- PostingFolder/CameraScreen.js
+++ PostingFolder/CameraScreen.js
@@ -359,6 +359,12 @@ class CameraScreen extends React.Component{
     this.props.navigation.goBack();
   }
 
+  onSelectGoal = (goal) => {
+    this.setState({
+      selectedGoal: goal
+    })
+  }
+
   render(){
 
     return(
@@ -451,6 +457,7 @@ class CameraScreen extends React.Component{
                   data = {this.state.goals}
                   showGoals = {this.state.showGoals}
                   onClose = {this.closeShowGoals}
+                  select = {this.onSelectGoal}
                   />
 
                <TouchableOpacity
@@ -472,32 +479,83 @@ class CameraScreen extends React.Component{
                    width = {40}/>
                </TouchableOpacity>
 
-               <TouchableOpacity
-                 onPress = {() => this.openShowGoals()}
-                 style ={{
-                   // position: 'relative',
-                   position: 'absolute',
-                   top: '5%',
-                   right:'5%',
-                   // top: 100
-                 }}
-                 >
-                 <Circle
-                   style={{top:0, position:'absolute'}}
-                   stroke = "#d9d9d9"
-                   strokeWidth={3}
-                   width = {40}
-                   height = {40}
-                    />
+               {
+                 !this.state.selectedGoal.goal  ?
 
-                <Circle
-                  strokeWidth={2}
-                  stroke = "white"
-                  width = {40}
-                  height = {40}
-                   />
+                 <View
+                   style ={{
+                     // position: 'relative',
+                     position: 'absolute',
+                     top: '4%',
+                     right: '2%',
+                     alignSelf: 'flex-end',
 
-               </TouchableOpacity>
+                     backgroundColor: '#000000aa',
+                     padding: 20,
+                     borderRadius: 30,
+                     flexDirection: 'row'
+                     // top: 100
+                   }}
+                   >
+
+
+
+                   <View>
+                     <Text style = {{
+                         color: 'white',
+                         textAlign: 'right'
+                       }}>these are</Text>
+                   </View>
+
+                   <View style = {{
+                       height: 20,
+                       width: 40
+                     }}>
+
+                   </View>
+
+                 </View>
+
+                 :
+
+                 null
+
+
+
+
+               }
+
+                 <TouchableOpacity
+                   onPress = {() => this.openShowGoals()}
+                   style ={{
+                     // position: 'relative',
+                     position: 'absolute',
+                     top: '5%',
+                     right:'5%',
+                     // top: 100
+                   }}
+                   >
+                   <Circle
+                     style={{top:0, position:'absolute'}}
+                     stroke = "#d9d9d9"
+                     strokeWidth={3}
+                     width = {40}
+                     height = {40}
+                      />
+
+                    <Circle
+                      strokeWidth={2}
+                      stroke = "white"
+                      width = {40}
+                      height = {40}
+                       />
+
+                 </TouchableOpacity>
+
+
+
+
+
 
                <TouchableOpacity
                  style = {styles.submitBtn}
