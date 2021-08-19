8c990f23c20a75b8dd9fda0a01f00703f63aa079
andybrvt <andybrvt@gmail.com>
2021-08-18 22:58:12 -0700

    stuff

---
+++ GoalAlbum/GoalPage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Text,
+   View,
+   Button,
+   StyleSheet,
+   Dimensions,
+   Image,
+   TouchableOpacity,
+} from 'react-native';
+
+class GoalPage extends React.Component{
+
+
+
+  render(){
+
+    return(
+      <View>
+        <Text>This will be the new goal page</Text>
+      </View>
+    )
+  }
+}
+
+export default GoalPage;
