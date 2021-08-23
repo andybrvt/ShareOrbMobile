f35b0471e81a8fb85ccf4873c7cd5163e41faa16
andybrvt <andybrvt@gmail.com>
2021-08-23 15:44:56 -0700

    invite page set up

--- Explore/Explore.js
+++ Explore/Explore.js
@@ -24,6 +24,7 @@ import TrendingList from './TrendingList';
 import SuggestedList from './SuggestedList';
 import Animated from 'react-native-reanimated';
 import SearchResults from './SearchResults';
+import InvitePage from './InvitePage';
 const { Value } = Animated;
 
 const {interpolate, Extrapolate, diffClamp, cond, lessOrEq} = Animated;
@@ -189,6 +190,7 @@ class Explore extends React.Component{
 
         <View style = {{
             flex: 1,
+            backgroundColor: '#1890ff'
           }}>
 
 
@@ -224,12 +226,7 @@ class Explore extends React.Component{
 
             :
 
-            <SuggestedList
-              loadMore = {this.loadMore}
-              navigation = {this.props.navigation}
-              y = {this.y}
-              cells = {exploreCells}
-              />
+            <InvitePage />
 
 
           }
---
+++ Explore/InvitePage.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import {
+  Text,
+  Platform,
+  SafeAreaView,
+  View,
+  Button,
+  StyleSheet,
+  ScrollView,
+  Dimensions,
+  TouchableOpacity,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard
+ } from 'react-native';
+import TextInputError from '../RandomComponents/TextInputError'
+
+const email = value =>
+  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
+    ? 'Invalid email address'
+    : undefined
+
+class InvitePage extends React.Component{
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      emailError: [],
+      email: ''
+    }
+  }
+
+  validate = (fieldName, value) => {
+
+    if(fieldName === "email"){
+
+      if(email(value)){
+        return "Not a valid email"
+      }
+    }
+
+
+
+  }
+
+
+   render(){
+
+     return(
+
+       <View style = {styles.container}>
+
+
+
+         <TouchableWithoutFeedback
+           style = {{flex: 1}}
+           onPress = {() =>Keyboard.dismiss()}
+           >
+           <View style = {{
+              flex: 1,
+              alignItems: 'center',
+              width: '100%'}}>
+
+
+              <View style = {{
+                  top: '20%'
+                }}>
+                <Text style = {{
+                  color: 'white',
+                  fontSize:30,
+                  // textShadowColor: 'rgba(0, 0, 0, 0.75)',
+                  // textShadowColor: 'black',
+                  // textShadowOffset: {width: -1, height: 1},
+                  // textShadowRadius: 5,
+                  fontWeight:'bold',
+                  }}>Invite a friend</Text>
+              </View>
+
+             <View style ={{
+                 width: '80%',
+                 top: '30%'
+               }}>
+
+               <TextInputError
+                 onBlur = {() => this.setState({
+                   emailError: this.validate("email", this.state.email)})}
+                 onChangeText = {(value) => this.setState({email: value.trim()})}
+                 placeholder = "Email"
+                 error = {this.state.emailError}
+
+                 />
+             </View>
+
+             <View style = {{
+                 width: '80%',
+                 alignItems: 'center',
+                 top: '40%'
+               }}>
+               <TouchableOpacity
+                  style = {styles.loginBtn}>
+                 <Text style = {styles.loginText}> Login</Text>
+               </TouchableOpacity>
+             </View>
+
+           </View>
+
+         </TouchableWithoutFeedback>
+
+       </View>
+
+     )
+   }
+ }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  loginBtn: {
+    position: "absolute",
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: 9999,
+    backgroundColor: "white",
+  },
+})
+
+
+export default InvitePage;
