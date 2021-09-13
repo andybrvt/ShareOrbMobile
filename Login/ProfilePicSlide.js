df93b1d8ad57688454944ed860d584532803af15
andybrvt <andybrvt@gmail.com>
2021-09-12 20:30:56 -0700

    bottom sheet

--- Login/ProfilePicSlide.js
+++ Login/ProfilePicSlide.js
@@ -8,7 +8,8 @@ import {
   Dimensions,
   Modal,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
+  TouchableWithoutFeedback
  } from 'react-native';
 import { Avatar } from 'react-native-elements';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
@@ -23,9 +24,47 @@ const height = Dimensions.get("window").height
 
 class ProfilePicSlide extends React.Component{
 
+  constructor(props){
+    super(props)
+    this.bs = React.createRef()
+  }
   next = () => {
 
     this.props.openModal(this.props.openNum)
+  }
+
+  renderHeader = () => (
+    <View style={styles.test}>
+    <View style={styles.header}>
+      <View style={styles.panelHeader}>
+        <View style={styles.panelHandle} />
+      </View>
+    </View>
+    </View>
+  );
+
+  renderInner =()=> {
+    return(
+      <View style={styles.panel}>
+
+        <TouchableOpacity
+          onPress = {this.handleTakeProfile}
+
+          style={styles.panelButton} >
+          <Text style={styles.panelButtonTitle}>Take Photo</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress = {this.handleChooseProfile}
+          style={styles.panelButton}>
+          <Text style={styles.panelButtonTitle}>Choose From Library</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.panelButton1}
+          onPress={() => this.bs.current.snapTo(1)}>
+          <Text style={styles.panelButtonTitle1}>Cancel</Text>
+        </TouchableOpacity>
+      </View>
+    )
   }
 
   render(){
@@ -42,14 +81,18 @@ class ProfilePicSlide extends React.Component{
           <Text style = {styles.textInput}>Pick a profile picture</Text>
         </View>
 
+        <TouchableWithoutFeedback
+          onPress={() => this.bs.current.snapTo(0)}
+          >
+          <View style = {styles.midContainer}>
+            <Avatar
+              size = {150}
+              rounded
+              source = {pic}
+               />
+          </View>
 
-        <View style = {styles.midContainer}>
-          <Avatar
-            size = {150}
-            rounded
-            source = {pic}
-             />
-        </View>
+        </TouchableWithoutFeedback>
 
 
 
@@ -82,7 +125,7 @@ class ProfilePicSlide extends React.Component{
 
           <BottomSheet
            ref={this.bs}
-           snapPoints={[330, 0]}
+           snapPoints={[250, 0]}
            renderContent={this.renderInner}
            renderHeader={this.renderHeader}
            initialSnap={1}
@@ -133,7 +176,60 @@ const styles = StyleSheet.create({
     alignItems: 'flex-end',
     justifyContent: 'center',
     paddingRight: 30
-  }
+  },
+  panel: {
+    padding: 20,
+    backgroundColor: '#FFFFFF',
+    paddingTop: 20,
+    // borderTopLeftRadius: 20,
+    // borderTopRightRadius: 20,
+    // shadowColor: '#000000',
+    // shadowOffset: {width: 0, height: 0},
+    // shadowRadius: 5,
+    // shadowOpacity: 0.4,
+  },
+  panelButtonTitle: {
+    color:'white'
+  },
+  panelButton: {
+   padding: 13,
+   borderRadius: 10,
+   backgroundColor: '#1890ff',
+   alignItems: 'center',
+   marginVertical: 7,
+   },
+   panelButton1: {
+    padding: 13,
+    borderRadius: 10,
+    backgroundColor: '#F0F0F0',
+    alignItems: 'center',
+    marginVertical: 7,
+    },
+    test: {
+      elevation:5,
+    },
+     header: {
+       backgroundColor: '#FFFFFF',
+       shadowColor: '#333333',
+       shadowOffset: {width: -1, height: -3},
+       shadowRadius: 2,
+       shadowOpacity: 0.2,
+
+       paddingTop: 20,
+       borderTopLeftRadius: 20,
+       borderTopRightRadius: 20,
+     },
+     panelHeader: {
+       // backgroundColor:'blue',
+       alignItems: 'center',
+     },
+     panelHandle: {
+       width: 40,
+       height: 8,
+       borderRadius: 4,
+       backgroundColor: '#00000040',
+       marginBottom: 10,
+     },
 })
 
 
