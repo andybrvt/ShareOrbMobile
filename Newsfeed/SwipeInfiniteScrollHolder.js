80c61993fee8a7184d7c31ac0a9611563233787d
andybrvt <andybrvt@gmail.com>
2021-09-19 20:41:00 -0700

    progress on tab view

--- Newsfeed/SwipeInfiniteScrollHolder.js
+++ Newsfeed/SwipeInfiniteScrollHolder.js
@@ -15,6 +15,15 @@ import { Avatar } from 'react-native-elements';
 import ScrollableTabBarNew from './ScrollableTabBarNew';
 import ScrollableTabView, { ScrollableTabBar } from 'react-native-scrollable-tab-view';
 
+class Page extends React.Component {
+    render() {
+        return (
+            <View style={{flex: 1, backgroundColor: 'blue'}}>
+                <Text>{this.props.name}</Text>
+            </View>
+        )
+    }
+}
 
 class SwipeInfiniteScrollHolder extends React.Component{
 
@@ -23,25 +32,49 @@ class SwipeInfiniteScrollHolder extends React.Component{
 
     return(
 
-      <ScrollableTabView
-        style={{ marginTop: 20 }}
-         initialPage={0}
-         renderTabBar={() => <ScrollableTabBarNew />}
-        >
-        <Text tabLabel='Tab #1'>My</Text>
-        <Text tabLabel='Tab #2'>favorite</Text>
-        <Text tabLabel='Tab #3'>project</Text>
-        <Text tabLabel='Tab #4'>favorite</Text>
-         <Text tabLabel='Tab #5'>project</Text>
-           <Text tabLabel='Tab #1'>My</Text>
-           <Text tabLabel='Tab #2'>favorite</Text>
-           <Text tabLabel='Tab #3'>project</Text>
-           <Text tabLabel='Tab #4'>favorite</Text>
-            <Text tabLabel='Tab #5'>project</Text>
+      <ScrollView>
+        <View style = {{flex: 1, height: 200}}>
+          <ScrollableTabView
+            style={{ marginTop: 20 }}
+             initialPage={0}
+             renderTabBar={() => <ScrollableTabBarNew />}
+            >
+            <Page name='tab1' tabLabel='tab1' />
+             <Page name='tab2' tabLabel='tab2' />
+             <Page name='tab3' tabLabel='tab3' />
+             <Page name='tab4' tabLabel='tab4' />
+             <Page name='tab5' tabLabel='tab5' />
+             <Page name='tab6' tabLabel='tab6' />
+          </ScrollableTabView>
 
-      </ScrollableTabView>
+        </View>
+
+
+      </ScrollView>
+
+
     )
   }
 }
+
+const styles = StyleSheet.create({
+  tabView: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: 'rgba(0,0,0,0.01)',
+  },
+  card: {
+    borderWidth: 1,
+    backgroundColor: '#fff',
+    borderColor: 'rgba(0,0,0,0.1)',
+    margin: 5,
+    height: 150,
+    padding: 15,
+    shadowColor: '#ccc',
+    shadowOffset: { width: 2, height: 2, },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+  },
+});
 
 export default SwipeInfiniteScrollHolder;
