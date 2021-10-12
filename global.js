9c991acd3b30947926ea1df6279c387c71dba094
pinghsu520 <pinghsu520@gmail.com>
2021-10-11 19:36:48 -0700

    nadroid version

--- global.js
+++ global.js
@@ -20,12 +20,12 @@ import * as Notifications from 'expo-notifications';
 
 
 // Ping UACI
-// global.IP_CHANGE = "http://206.207.51.166:19002"
-// global.IMAGE_ENDPOINT = "http://206.207.51.166:19002"
-// global.WS_ENDPOINT = "206.207.51.166:19002"
-// global.POSTLIST_SPEC = 'http://206.207.51.166:19002/media/'
-// global.WS_HEADER = "ws"
-// console.disableYellowBox = true;
+global.IP_CHANGE = "http://206.207.51.166:19002"
+global.IMAGE_ENDPOINT = "http://206.207.51.166:19002"
+global.WS_ENDPOINT = "206.207.51.166:19002"
+global.POSTLIST_SPEC = 'http://206.207.51.166:19002/media/'
+global.WS_HEADER = "ws"
+console.disableYellowBox = true;
 
 
 
@@ -112,11 +112,11 @@ import * as Notifications from 'expo-notifications';
 
 
 // Andy UACI
-global.IP_CHANGE = "http://206.207.51.84:19000"
-global.IMAGE_ENDPOINT = "http://206.207.51.84:19000"
-global.WS_ENDPOINT = "206.207.51.84:19000"
-global.POSTLIST_SPEC = 'http://206.207.51.84:19000/media/'
-global.WS_HEADER = "ws"
+// global.IP_CHANGE = "http://206.207.51.84:19000"
+// global.IMAGE_ENDPOINT = "http://206.207.51.84:19000"
+// global.WS_ENDPOINT = "206.207.51.84:19000"
+// global.POSTLIST_SPEC = 'http://206.207.51.84:19000/media/'
+// global.WS_HEADER = "ws"
 
 
 // global.IP_CHANGE = "http://206.207.51.82:19002"
@@ -228,7 +228,7 @@ global.FILE_NAME_GETTER = (fileURI) => {
 global.SEND_GROUP_COMMENT_NOTIFICATION = async(
   expoPushToken,
   sender,
-  postId // its not the group id so fix it later
+  groupId // its not the group id so fix it later
 ) => {
   if(expoPushToken !== ""){
     const message = {
@@ -238,7 +238,7 @@ global.SEND_GROUP_COMMENT_NOTIFICATION = async(
       body: 'Click to check it out!',
       data: {
         type: "group_comment",
-        postId: postId,
+        groupId: groupId,
        },
     }
 
@@ -260,9 +260,8 @@ global.SEND_GROUP_COMMENT_NOTIFICATION = async(
 global.SEND_GROUP_LIKE_NOTIFICATION = async(
   expoPushToken,
   sender,
-  postId,
+  groupId,
 ) => {
-  console.log(expoPushToken, sender, postId, 'stuf stuff')
   if(expoPushToken !== ""){
     const message = {
       to: expoPushToken,
@@ -271,7 +270,7 @@ global.SEND_GROUP_LIKE_NOTIFICATION = async(
       body: 'Click to check it out!',
       data: {
         type: "group_like",
-        postId: postId,
+        groupId: groupId,
        },
     }
 
